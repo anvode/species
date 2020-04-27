2a958d8c17be8964a7ad77ac7d98435f929623a8
Naci Akce <n.akce@limesoda.com>
2020-04-27 20:32:08 +0200

    remove sw and add scss support

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
@@ -20,6 +20,10 @@ function App() {
         </a>
       </header>
     </div>
+
+
+
+
   );
 }
 
